77e1a7e0159c775e98272a2edf95e1955aaeddc3
Geoffrey Baldry <geoffrey.baldry@hotmail.com>
2025-09-29 14:08:45 +0100

    Projectile queue_free test

--- scripts/projectiles/projectile_bullet.gd
+++ scripts/projectiles/projectile_bullet.gd
@@ -11,15 +11,30 @@ var fired_by: Node
 func _ready() -> void:
 	NetworkTime.on_tick.connect(_tick)
 
+
 func _tick(delta, _t):
 	position += transform.x * SPEED * delta
 
+
+func remove_projectile() -> void:
+	queue_free()
+	
+	NetworkTime.on_tick.disconnect(_tick)
 
 func _on_body_entered(body: Node2D) -> void:
 	Log.pr("Projectile entered body : " + str(body.name))
 	if body.is_in_group("enemies"):
 		Log.pr("Peer ID " + str(fired_by.peer_id) + " shot an enemy!")
+		# TBD - award points to the player
+	
+	remove_projectile()
 
 
 func _on_area_entered(area: Area2D) -> void:
 	Log.pr("Projectile entered area : " + str(area.name))
+	remove_projectile()
+
+
+func _on_timer_timeout() -> void:
+	Log.pr("Projectile timed out after not colliding.")
+	remove_projectile()
