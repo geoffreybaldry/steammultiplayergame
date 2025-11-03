8c5c09f3b353d3a29ef8e14d93b73d03d455f822
Geoffrey Baldry <geoffrey.baldry@hotmail.com>
2025-11-03 08:00:57 +0000

    Creating base projectile class

---
+++ scripts/projectiles/projectile.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name Projectile
+
+@export var speed:int
+@export var direction:Vector2
+@export var damage:int
+
+var peer_id:int
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
